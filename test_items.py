d69d23936c1be4bb7db8bf61860753057b0a81b0
kodi1993 <Akhmedka1993@gmail.com>
2025-06-21 16:03:05 +0600

    Add wait for visual inspection

--- test_items.py
+++ test_items.py
@@ -2,11 +2,14 @@ import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_buy_button_exist(browser):
     browser.get(link)
+
+    time.sleep(5)
 
     button = WebDriverWait(browser, 10).until(
         EC.visibility_of_element_located((By.CSS_SELECTOR, "button.btn-add-to-basket"))
